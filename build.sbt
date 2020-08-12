2ef35da6a22c0412079f5c438c91d59b4936da3c
Arnout Engelen <arnout@bzzt.net>
2020-08-12 15:28:06 +0000

    Add a gRPC service
    Needs https://github.com/lightbend/cloudflow/pull/626

--- build.sbt
+++ build.sbt
@@ -1,12 +1,25 @@
 import sbt._
 import sbt.Keys._
 
-lazy val experiment = (project in file("."))
-  .enablePlugins(CloudflowApplicationPlugin, CloudflowAkkaPlugin)
-  .settings(
-    scalaVersion := "2.12.11",
-    runLocalConfigFile := Some("src/main/resources/local.conf"),
-    libraryDependencies ++= Seq(
-      "com.typesafe.akka" %% "akka-http2-support"      % "10.1.12",
-    )
-  )
+enablePlugins(
+  CloudflowApplicationPlugin,
+  CloudflowAkkaPlugin,
+  AkkaGrpcPlugin
+)
+
+//akkaGrpcGeneratedSources := List(AkkaGrpc.Server)
+
+// CommonSettingsAndTasksPlugin and AkkaGrpcPlugin both contribute
+// ScalaPB code generation to the PB.targets, but with a different
+// target directory, causing compiling the second class to fail because
+// the first already existed (even though they are identical).
+// For now, we explicitly remove the duplicate
+PB.targets in Compile ~= (f => {
+  f.filter(!_.outputPath.getName.contains("scalapb"))
+  })
+
+scalaVersion := "2.12.11"
+//runLocalConfigFile := Some("src/main/resources/local.conf")
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-http2-support"      % "10.1.12",
+)
--- src/main/scala/SensorDataHttpIngress.scala
+++ src/main/scala/SensorDataHttpIngress.scala
@@ -1,21 +1,36 @@
 package sensordata
 
+import akka.NotUsed
+import akka.grpc.scaladsl.ServerReflection
+import akka.grpc.scaladsl.ServiceHandler
 import akka.http.scaladsl.{Http, Http2}
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse, StatusCodes}
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.settings.ServerSettings
-import akka.stream.scaladsl.Flow
+import akka.stream.scaladsl.{Flow, Source}
 import cloudflow.akkastream._
 import cloudflow.akkastream.util.scaladsl._
 import cloudflow.streamlets._
 import cloudflow.streamlets.avro._
+import example.myapp.helloworld.grpc.{GreeterService, GreeterServiceHandler, HelloReply, HelloRequest}
+import grpc.reflection.v1alpha.reflection.ServerReflectionHandler
 
+import scala.concurrent.Future
 import scala.util.Failure
 
 class SensorDataHttpIngress extends AkkaServerStreamlet {
   def shape                = StreamletShape.empty
+
+  val implementation = new GreeterService {
+    override def sayHello(in: HelloRequest): Future[HelloReply] =
+      Future.successful(HelloReply(s"Good to see you using CloudFlow, ${in.name}"))
+    override def itKeepsTalking(in: Source[HelloRequest, NotUsed]): Future[HelloReply] = ???
+    override def itKeepsReplying(in: HelloRequest): Source[HelloReply, NotUsed] = ???
+    override def streamHellos(in: Source[HelloRequest, NotUsed]): Source[HelloReply, NotUsed] = ???
+  }
+
   override def createLogic = new HttpServerLogic(this) {
     override def route(): Route = complete(StatusCodes.ImATeapot)
 
@@ -23,7 +38,11 @@ class SensorDataHttpIngress extends AkkaServerStreamlet {
     override def run(): Unit = {
       Http()
         .bindAndHandleAsync(
-          Route.asyncHandler(route()),
+          ServiceHandler.concatOrNotFound(
+            GreeterServiceHandler.partial(implementation),
+            ServerReflection.partial(List(GreeterService))
+          ),
+          //Route.asyncHandler(route()),
           "0.0.0.0",
           containerPort,
           Http().defaultServerHttpContext
